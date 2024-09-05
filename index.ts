cd6de37a710dcac7b99facfe9e3500ce21733f3e
Dominic Ruggiero <dominic@userexe.me>
2024-09-04 21:33:47 -0400

    limit amount of projects & move some stuff around a bit

--- index.ts
+++ index.ts
@@ -23,19 +23,23 @@ Bun.serve({
         repo.description &&
         new Date(repo.created_at).getTime() > Date.now() - 31556952000
     );
-    repos = repos.map((r) => ({
-      ...r,
-      homepage: r.homepage
-        ? r.homepage.replace(/(https?:\/\/)|(\/$)/g, "")
-        : r.homepage,
-    }));
-    repos = repos.sort(
-      (a, b) =>
-        new Date(b.pushed_at).getTime() - new Date(a.pushed_at).getTime()
-    );
+    repos = repos
+      .map((r) => ({
+        ...r,
+        homepage: r.homepage
+          ? r.homepage.replace(/(https?:\/\/)|(\/$)/g, "")
+          : r.homepage,
+      }))
+      .sort(
+        (a, b) =>
+          new Date(b.pushed_at).getTime() - new Date(a.pushed_at).getTime()
+      )
+      .splice(0, 7);
 
     const projects = repos
-      .map((r) => `${r.homepage ? r.homepage : r.name}: ${r.description}`)
+      .map(
+        (r) => `${r.homepage ? r.homepage : r.name}: ${r.description?.trim()}`
+      )
       .join("\n");
 
     return new Response(
@@ -44,13 +48,13 @@ Bun.serve({
 I like working on cool code things and cool non-code things.
 I didn't like my website and didn't want to remake it, so now it's just plain text.
 
-My Github username is mrhappyma, and my email is my name at this domain.
-
 ${
   repos.length > 0 &&
   `Here's some stuff I've been working on recently:
 ${projects}`
-}`
+}
+
+My Github username is mrhappyma, and my email is my name at this domain.`
     );
   },
 });
